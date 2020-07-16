6ae3be76cb99afcf7e2d6252a346527688f58356
Malte L <38286424+parzival-games@users.noreply.github.com>
2020-07-16 21:36:23 +0200

    1.1.1

---
+++ plugins/raccon-heroku.js
@@ -0,0 +1,34 @@
+/**
+ * @name raccon-heroku.js
+ * @description Dieses Plugin ermöglicht es der Anwendung permanent aktiv zu bleiben.
+ * @author Parzival
+ * @version 1.0.0.0
+ */
+
+// Dieses Plugin benötigt weitere Module die noch nachgeladen werden müssen.
+const express = require('express');
+const app = express();
+
+/**
+ * @description Einstiegspunkt des Plugins
+ */
+module.exports.run = function() {
+
+    // Erstellt ein einfachen Website listener.
+    app.get('*', (req, res) => {
+        var response = "<html><head><title>raccoon-heroku.js</title><style>body, html {padding: 0;margin: 0;width: 100%;height: 100%;overflow: hidden;}* {font-family: monospace;color: white;background-color: black;}body {margin-left: 5px;}</style></head><body><h1>plugins/raccoon-heroku.js</h1><p>All requests are processed by the raccon-heroku.js plugin.</p><p>When this is not the desired effect, please check the installed plugins.</p><p>raccoon-heroku.js overwrites all created request handlers by default.</p></body></html>";
+        res.send(response);
+    });
+
+    // Startet den gefakten WebServer
+    app.listen(80);
+}
+
+/**
+ * @description Stellt wichtige initialisierungs Informationen für die einbindung des Plugins bereit. 
+ */
+module.exports.help = {
+    apiVersion: 1,
+    pluginType: "OVERWRITE",
+    name: "raccon-heroku"
+}
